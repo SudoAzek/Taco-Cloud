d0e18ad7a8740396b3e1bca78041da087f7ba9fe
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-10-07 12:39:38 +0900

    Update HomeControllerTest.java

--- src/test/java/com/mezonworks/tacocloud/tacos/HomeControllerTest.java
+++ src/test/java/com/mezonworks/tacocloud/tacos/HomeControllerTest.java
@@ -10,7 +10,6 @@ import static
 import static
         org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.mezonworks.tacocloud.tacos.HomeController;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
